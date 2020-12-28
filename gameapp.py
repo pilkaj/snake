8a697908cc7dc91452022e25330f9d30e535d9b6
Dominik Jašek <jasekdominik@seznam.cz>
2020-12-28 18:39:33 +0100

    added condition if __name__ == "__main__"

--- gameapp.py
+++ gameapp.py
@@ -1,13 +1,20 @@
-''' python my memory dust eraser application
-'''
-print("This is a memory dust eraser application")
-print("Let's try to write a snake game")
-
 from snakegame import SnakeGame
 
-''' play snake '''
-mygame = SnakeGame("graphical")
-(snake_length, game_time) = mygame.playgame()
+def main():
+  ''' 
+  python my memory dust eraser application
+  '''
+  print("This is a memory dust eraser application")
+  print("Let's try to write a snake game")
 
-print("You played a snake game.")
-print("Your snake reached length", snake_length, "in total time", game_time, "seconds.")
+  ''' play snake '''
+  mygame = SnakeGame("graphical")
+  (snake_length, game_time) = mygame.playgame()
+
+  print("You played a snake game.")
+  print("Your snake reached length", snake_length, "in total time", game_time, "seconds.")
+
+
+if __name__ == "__main__":
+  main()
+
